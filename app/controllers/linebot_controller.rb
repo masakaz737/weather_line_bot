35c860539848752e944ff7ef67d268a91b958757
ishida_masakazu <u225210b@gmail.com>
2018-11-14 22:51:37 +0900

    気温のセリフ追加

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -78,12 +78,16 @@ class LinebotController < ApplicationController
               push =
                 "今日の天気？\n今日は雨が降りそうだから傘があった方が安心だよ。\n　最高気温　#{max_temp}℃\n　最低気温　#{min_temp}℃\n　  6〜12時　#{per06to12}％\n　12〜18時　 #{per12to18}％\n　18〜24時　#{per18to24}％\n#{word}"
             else
-              word =
-                ["絶好の外気浴日和だね(^^)",
-                 "天気がいいからいつもとは違うサウナに足伸ばしてみる？",
-                 "天気がいい日もサウナ！"].sample
-              push =
-                "今日の天気？\n今日は雨は降らなさそうだよ。\n#{word}"
+              if min_temp <= 10
+                word1 = "今日は冷えるからサウナに行こう！"
+              else
+                word2 =
+                  ["絶好の外気浴日和だね(^^)",
+                   "天気がいいからいつもとは違うサウナに足伸ばしてみる？",
+                   "天気がいい日もサウナ！"].sample
+              end
+                push =
+                  "今日の天気？\n今日は雨は降らなさそうだよ。\n　最高気温　#{max_temp}℃\n　最低気温　#{min_temp}℃\n#{word1}\n#{word2}"
             end
           end
           # テキスト以外（画像等）のメッセージが送られた場合
